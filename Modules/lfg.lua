3809c428f926dd59315783810249632cec0fe807
xeropresence <3128949+xeropresence@users.noreply.github.com>
2018-12-19 22:10:49 -0500

    Update C_LFGList.GetApplicantInfo to use the new return values

--- Modules/lfg.lua
+++ Modules/lfg.lua
@@ -61,14 +61,14 @@ end
  
 function QuickLink_LFG:LFGListUtil_GetApplicantMemberMenu(applicantID, memberIdx)
     local name, class, localizedClass, level, itemLevel, tank, healer, damage, assignedRole = C_LFGList.GetApplicantMemberInfo(applicantID, memberIdx);
-    local id, status, pendingStatus, numMembers, isNew, comment = C_LFGList.GetApplicantInfo(applicantID);
+    local getApplicantInfo = C_LFGList.GetApplicantInfo(applicantID);
 	
 	local menu = self.hooks["LFGListUtil_GetApplicantMemberMenu"](applicantID, memberIdx);
 	
 	local applicantMenu = getLFGQuickLinkMenu();
 	for k, e in pairs(applicantMenu) do
 		e.arg1 = name;
-		e.disabled = not name or (status ~= "applied" and status ~= "invited");
+		e.disabled = not name or (getApplicantInfo.applicationStatus ~= "applied" and getApplicantInfo.applicationStatus ~= "invited");
 	end
 	
     return updateMenu(menu, applicantMenu);
