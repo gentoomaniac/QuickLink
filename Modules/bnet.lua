2b27733fd2d669a27ceaf5364b9950457bdb5cc7
Marco Siebecke <gentoomaniac@gmail.com>
2021-01-11 13:03:43 +0100

    adopted to api changes in Shadowlands

--- Modules/bnet.lua
+++ Modules/bnet.lua
@@ -3,9 +3,10 @@ local QuickLink_BNET = QuickLink:NewModule("QuickLink_BNET", "AceConsole-3.0", "
 local L = LibStub("AceLocale-3.0"):GetLocale("QuickLink", true)
 
 -- There must be a better way!
-function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount)
+function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount, communityClubID, communityStreamID, communityEpoch, communityPosition, mobile)
 	local index = nil
-	local _, _, _, _, _, _, client, _, _, _, _, _, _, _, _, _, _, _ = BNGetFriendInfoByID(bnetIDAccount)
+
+	local accountInfo = C_BattleNet.GetAccountInfoByID(bnetIDAccount)
 
 	for i, value in pairs(UnitPopupMenus["BN_FRIEND"]) do
 		if value == "QUICKLINK" then
@@ -13,7 +14,7 @@ function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, cha
 		end
 	end
 
-	if client == BNET_CLIENT_WOW then
+	if accountInfo.gameAccountInfo.clientProgram == BNET_CLIENT_WOW then
 		if index == nil then
 			table.insert(UnitPopupMenus["BN_FRIEND"], #(UnitPopupMenus["BN_FRIEND"]), "QUICKLINK");
 		end
@@ -23,7 +24,7 @@ function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, cha
 		end
 	end
 
-	self.hooks["FriendsFrame_ShowBNDropdown"](name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount);
+	self.hooks["FriendsFrame_ShowBNDropdown"](name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount, communityClubID, communityStreamID, communityEpoch, communityPosition, mobile);
 end
 
 function QuickLink_BNET:OnEnable()
